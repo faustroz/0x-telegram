2de4e6656f951a9e24fe6329b8081ee85cdcc7f5
faustro <ferdysuper171@gmail.com>
2025-02-17 23:23:41 +0800

    Neaty

--- commands/student.js
+++ commands/student.js
@@ -9,15 +9,12 @@ module.exports = {
     const filePath = "qrcode.png";
 
     try {
-      // Generate QR Code dan simpan sebagai file sementara
       await QRCode.toFile(filePath, textToConvert);
 
-      // Kirim gambar QR Code ke pengguna
       await bot.sendPhoto(chatId, filePath, {
         caption: `✅ QR Code for: ${textToConvert}\n\n🤖 Bot by Made Ferdy Diatmika`,
       });
 
-      // Hapus file setelah dikirim
       fs.unlinkSync(filePath);
     } catch (err) {
       bot.sendMessage(chatId, "❌ Failed to generate QR Code.");
